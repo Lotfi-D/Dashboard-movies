824f9e6fed7dcbca818472930d8ef199c4829c34
unknown <lotfidoucanef@hotmail.fr>
2023-09-30 14:07:49 +0200

    install pinia, element plus, tailwind and the header has been added

--- front-dashboard/src/main.ts
+++ front-dashboard/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia';
 import router from './router'
+import { ElLoading } from 'element-plus';
+import 'element-plus/dist/index.css';
+import 'tailwindcss/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).use(ElLoading).mount('#app')
