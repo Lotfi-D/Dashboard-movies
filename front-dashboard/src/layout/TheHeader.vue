e3c2a83d0865d6d0a81f4768ba86828a9b1ec534
Lotfi Doucanef <ldoucanef@weborama.com>
2023-10-03 13:11:33 +0200

    font bold in header

--- front-dashboard/src/layout/TheHeader.vue
+++ front-dashboard/src/layout/TheHeader.vue
@@ -1,10 +1,10 @@
 <template>
-  <header class="bg-black p-4 w-full z-10">
+  <header class="bg-black p-4 w-full z-10 font-bold">
     <div class="container flex items-center">
       <router-link to="/">
         <img
           src="../../public/static/logo.png"
-          alt="Netflix Logo"
+          alt="logo"
           class="h-12"
         />
       </router-link>
--- front-dashboard/tailwind.config.js
+++ front-dashboard/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['"PT Sans"', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
